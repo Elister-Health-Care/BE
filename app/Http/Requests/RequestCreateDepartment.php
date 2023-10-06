cad926319064be8ca6af64b34093a8e80f599809
Nguyễn Văn Mạnh <81563451+NguyenVanManh-AI@users.noreply.github.com>
2023-10-06 10:48:18 +0700

    Validate

--- app/Http/Requests/RequestCreateDepartment.php
+++ app/Http/Requests/RequestCreateDepartment.php
@@ -39,6 +39,7 @@ class RequestCreateDepartment extends FormRequest
             'success' => false,
             'message' => 'Validation errors',
             'data' => $errors,
+            'errors' => $validator->errors(),
             'status' => 400,
         ], 400));
     }
